e48ba89399020e3a60e7e720216eceaea74f8564
Francisco Costa <francisco100eg@gmail.com>
2022-10-07 18:41:55 -0300

    Requisito 2

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { Route, Switch } from 'react-router-dom';
 // import logo from './trivia.png';
 import './App.css';
 import Login from './Pages/Login';
+import Questions from './Pages/Questions';
 // Grupo 5
 export default function App() {
   return (
     <Switch>
       <Route exact path="/" component={ Login } />
+      <Route exact path="/questions" component={ Questions } />
     </Switch>
     // <div className="App">
     //   <header className="App-header">
